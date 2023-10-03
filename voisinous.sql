4ba832b08ab55b682153cf5fb7c479e24e09d197
MargotHuet <margothuet4@gmail.com>
2023-10-03 10:31:21 +0200

    firstcommit

--- voisinous.sql
+++ voisinous.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `voisinous`
 --
-
+CREATE DATABASE IF NOT EXISTS `voisinous` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `voisinous`;
 -- --------------------------------------------------------
 
 --
