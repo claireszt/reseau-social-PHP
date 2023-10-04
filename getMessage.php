3865de327bc9c7b0c4887b1be69d97128c1219de
MargotHuet <margothuet4@gmail.com>
2023-10-04 16:10:29 +0200

    /

--- getMessage.php
+++ getMessage.php
@@ -4,10 +4,12 @@
         <meta charset="utf-8">
         <title>ReSoC - Flux</title>         
         <meta name="author" content="Julien Falconnet">
-        <link rel="stylesheet" href="style.css"/>
+        <link rel="stylesheet" href="/htmlcss/stylesheets/_body.css"/>
     </head>
     <body>
-    <?php include ("./header.php"); ?>
+
+        <?php include("htmlcss/navbar.php")?> <!-- import navbar -->
+        <?php include("htmlcss/index.html")?> <!-- import index -->
 
     <?php
     // Connection à la base de données
@@ -19,8 +21,8 @@
         echo("Échec de la connexion : " . $mysqli->connect_error);
         exit();
     } else {
-        if(isset($_POST['envoyer']))
-        echo "<pre>" . print_r($_POST["content"]) . "</pre>";
+        // vérification
+        if(isset($_POST['envoyer']), isset($_POST['pseudo']))
                 {
                     $content= $_POST["content"];
                     $userid = '1'; // $userid = $_POST["userid"];
@@ -28,7 +30,7 @@
                     $groupeid = '4'; // $groupeid = $_POST["groupeid"];
                     $principale = '1'; // $principale = $_POST["principale"]; 
 
-
+                    // Insertion des données dans la table "Posts".
                     $queryInsertMessage = "INSERT INTO Posts (content, userid, date, groupeid, principale) "
                     . "VALUES ('" 
                     . $content . "',"
@@ -37,6 +39,7 @@
                     . $groupeid . "',"
                     . "'" . $principale . "');";
 
+                    // Vérfication
                     $createmessage = $mysqli->query($queryInsertMessage);
                     if ($createmessage === TRUE) {
                         echo "Données insérées avec succès.";
@@ -44,18 +47,20 @@
                         echo "Erreur lors de l'insertion : " . $mysqli->error;
                     }
 
+                    // Récupération des données de la colonne "content" dans la table "Posts"
                     $displayAllMessages = "SELECT `content` FROM Posts";
                         $lesInformations = $mysqli->query($displayAllMessages);
                         $displayMessage = $lesInformations->fetch_assoc();
                     }
                 }
                 ?>
-
-                <form action="messagefeature.php" method="POST">
+                
+                <!-- Mise en page et appel du message -->
+                <form action="getMessage.php" method="POST">
                     <label for="message">Votre message: </label>
                     <input type=text name="content" id="message" placeholder="type something..." required></input>
                     <button type=submit value="envoyer" name="envoyer">Envoyer</button>
-                    <p style="color:white;"><?php echo $displayAllMessages ?></p>
+                    <p><?php echo $_POST["content"] ?></p>  <!-- Affichage dans le html -->
                 </form>
                
     </body>
