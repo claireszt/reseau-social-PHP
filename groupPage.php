99483d47fbf5191cced359589d8bb5180f17e58c
Lachignol <110435478+Lachignol@users.noreply.github.com>
2023-10-05 16:16:35 +0200

    ajoutrequetegroupe

--- groupPage.php
+++ groupPage.php
@@ -1,3 +1,29 @@
+<?php 
+$mysqli = new mysqli("localhost", "root", "root", "voisinous");
+    //verification
+    if ($mysqli->connect_errno){
+        echo("Échec de la connexion : " . $mysqli->connect_error);
+        exit();
+    }
+    else{
+        $groupId = "2";
+        $querySearchGroup = "SELECT * "
+        . "FROM groupes "
+        . "WHERE "
+        . "id = '" . $groupId . "'";
+        $searchGroup = $mysqli->query($querySearchGroup);
+        $result = $searchGroup->fetch_assoc();
+        if($result != null){
+            print_r($result);
+        }
+        else{
+            $errorMessage = 'Aucun groupe a cette localisation !';
+        }
+        }
+        
+        ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,17 +46,12 @@
                     src="https://scontent-cdg4-3.xx.fbcdn.net/v/t39.30808-6/295177885_469072138554182_3136524954159481461_n.png?_nc_cat=106&ccb=1-7&_nc_sid=a2f6c7&_nc_ohc=lr7_QKzipPwAX-JIw3E&_nc_ht=scontent-cdg4-3.xx&cb_e2o_trans=t&oh=00_AfBCbK-3QSZDGUaUXKm9ni9KPU_qhcC1CyCl6vMPKTh9Qw&oe=6522828A" />
                 <div>
                     <div>
-                        <h3>Les bricoleurs du dimanche</h3>
-                        <h5>créé le 4 octobre 2023</h5>
-                        <span>75011</span>
+                        <h3> <?php echo $result['name']?> </h3><br />
+                        <span><?php echo $result['localisation']?></span>
                     </div>
                 </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget dapibus libero, quis vulputate
-                    leo. Nunc in sem pharetra, dictum mauris eu, dapibus sem. Phasellus quis porttitor velit, eget
-                    laoreet justo. Ut quam nisl, cursus et ante in, lacinia congue sapien. Nam vel tortor tincidunt,
-                    ornare mi quis, tristique ipsum. Nulla facilisi. Vestibulum at mi a sem efficitur mattis. Sed
-                    finibus nulla ex.</p>
-                <h3>Membres (3)</h3>
+                <p><?php echo $result['decription']?></p>
+                <h3>Membres</h3>
                 <ul>
                     <li>Gérard</li>
                     <li>Michel</li>
