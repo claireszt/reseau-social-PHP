2572982e1df80f83af3ad498000bad4a107fa217
onat68 <onat.rigault@gmail.com>
2023-10-06 11:36:45 +0200

    coucou

--- functionCalculDistance.php
+++ functionCalculDistance.php
@@ -6,20 +6,27 @@
 
 // $createGroupe = $queryCreateGroup->execute();
 
-function toRad($value){
+function toRad($value)
+{
     return $value * pi() / 180;
 }
-function calcDistance($lat1, $lon1, $lat2, $lon2){
+function calculDistance($lat1, $lon1, $lat2, $lon2)
+{
     $R = 6371; // km
-    $dLat = toRad($lat2-$lat1);
-    $dLon = toRad($lon2-$lon1);
+    $dLat = toRad($lat2 - $lat1);
+    $dLon = toRad($lon2 - $lon1);
     $lat1 = toRad($lat1);
     $lat2 = toRad($lat2);
 
-    $a = sin($dLat/2) * sin($dLat/2) +sin($dLon/2) * sin($dLon/2) * cos($lat1) * cos($lat2); 
-    $c = 2 * atan2(sqrt($a), sqrt(1-$a)); 
+    $a = sin($dLat / 2) * sin($dLat / 2) + sin($dLon / 2) * sin($dLon / 2) * cos($lat1) * cos($lat2);
+    $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
     $d = $R * $c;
+
     return $d;
+}
+
+function sortByDist($group) {
+
 }
 
 $mysqli = new mysqli("localhost", "root", "root", "voisinous");
@@ -28,28 +35,29 @@ if ($mysqli->connect_errno) {
     echo ("Échec de la connexion : " . $mysqli->connect_error);
     exit();
 } else {
+
     $queryUserGeoloc = "SELECT * FROM users WHERE id=8;";
     $userGeoloc = $mysqli->query($queryUserGeoloc);
-    foreach ($userGeoloc as $user) {
-        $userLatitude = $user['latitude'];
-        $userLongitude = $user['longitude'];
-    }
+    $user = $userGeoloc->fetch_array();
 
     $queryAllGroups = "SELECT * FROM Groupes";
     $allGroups = $mysqli->query($queryAllGroups);
-    $groupsToCheck = array();
-    foreach ($allGroups as $group) {
-        $groupToCheck['latitude'] = $group['latitude'];
-        $groupToCheck['longitude'] = $group['longitude'];
-        array_push($groupsToCheck, $groupToCheck);
+    print_r($allGroups);
+    while($row = $allGroups->fetch_assoc()) {
+        print_r($row);
+        $group = $row;
+    }
+    $groups = $allGroups->fetch_assoc();
+
+    foreach ($groups as $group) {
+        // print_r($group);
+        $group['distanceToUser'] = calculDistance($user['latitude'], $user['longitude'], $group['latitude'], $group['longitude']);
+        echo "<p>" . print_r($group) . "Le groupe " . $group['name'] . " est à " . $group['distanceToUser'] . " km</p>";
 
     }
 
-    foreach ($groupsToCheck as $group){
-        print_r($group);
-        $group['distanceToUser'] = calcdistance($userLatitude,$userLongitude,$group['latitude'],$group['longitude']);
-        echo "<p>" . "Le groupe est à " . $group['distanceToUser'] . "</p>" ;
-    }
+    print_r($groups);
+
 }
 
 
